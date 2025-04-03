8ecb496d52eb4afd6b1bd25a1cf6538909e9b5fd
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-04-04 01:00:39 +0300

    start

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,2 +1,32 @@
+// import Swiper from 'swiper';
+// import 'swiper/css';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// // import styles bundle
+// import 'swiper/css/bundle';
+// import 'swiper/css/keyboard';
+
+// init Swiper:
+const swiper = new Swiper();
+
+
+
 export function review(params) {}
 export default review;
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper(".mySwiper", {
+        slidesPerView: 4, // Показывать 1 карточку за раз
+        spaceBetween: 16, // Отступ между карточками
+        loop: true, // Бесконечная прокрутка
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+  
+  
+console.log("privet");
