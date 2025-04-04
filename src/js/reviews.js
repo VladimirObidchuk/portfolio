a9775e1533f475803ea59bddbe61063e97822e59
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-04-04 23:48:18 +0300

    markup-swiper-draft

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,8 @@
+export function review(params) {}
+export default review;
+
+
+
 // import Swiper from 'swiper';
 // import 'swiper/css';
 // import Swiper bundle with all modules installed
@@ -8,25 +13,56 @@ import Swiper from 'swiper/bundle';
 // import 'swiper/css/keyboard';
 
 // init Swiper:
+
+// import 'swiper/css';
+// import 'swiper/css/navigation';
 const swiper = new Swiper();
 
 
-
-export function review(params) {}
-export default review;
-
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
     new Swiper(".mySwiper", {
-        slidesPerView: 4, // Показывать 1 карточку за раз
-        spaceBetween: 16, // Отступ между карточками
-        loop: true, // Бесконечная прокрутка
+        // slidesPerView: "auto",
+        slidesPerView: 4,
+         // Показывать 1 карточку за раз
+        spaceBetween: 16,
+        // centeredSlides: false,
+        
+        // Отступ между карточками
+        // loop: true, 
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+
+
+        breakpoints: {
+                  // Когда ширина экрана >= 1024px
+                  1440: {
+                    slidesPerView: 4, // показывать 3 слайда
+                  },
+                  // Когда ширина экрана >= 768px
+                  768: {
+                    slidesPerView: 2, // показывать 2 слайда
+                  },
+                  // Когда ширина экрана < 768px
+                  0: {
+                    slidesPerView: 1, // показывать 1 слайд
+                  },
+                }
+        // breakpoints: {
+
+        //     320: {
+        //         slidesPerView: 1,
+        //       },
+        //     768: {
+        //         slidesPerView: 2, // На планшетах 2 картки
+        //     },
+            
+        //     1440: {
+        //         slidesPerView: 4, // На десктопі теж 3 картки
+        //     }
+        // }
     });
-});
+// });
 
   
-  
-console.log("privet");
