77ca2cbd31eea8667cce16cee506e8964a84c6fc
Denys <gozhievd@gmail.com>
2025-04-07 23:08:21 +0200

    correction

--- src/js/project.js
+++ src/js/project.js
@@ -1,20 +1,17 @@
 export function project(params) {}
 export default project;
 
-import Swiper from 'swiper';
-import { Navigation, Keyboard } from 'swiper/modules';
-
+import Swiper, { Navigation, Keyboard } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/keyboard';
 
 const projSwiper = new Swiper('.project-swiper', {
   modules: [Navigation, Keyboard],
-  loop: false,
+  loop: true,
   centeredSlides: true,
   slidesPerView: 1,
   spaceBetween: 10,
-
   keyboard: {
     enabled: true,
     onlyInViewport: false,
