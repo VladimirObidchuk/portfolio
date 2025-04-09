f3ecdbdd47a611ae1185db92fad3c38b4423e850
Vladimir <kremench@gmail.com>
2025-04-09 22:38:58 +0300

    fix reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,25 +1,21 @@
 import sprite from '../img/icon.svg';
 
-
-
 export const emptyRender = () => {
-
-  return `<p class="not-found-alert"> No information found</p>`
-
+  return `<p class="not-found-alert"> No information found</p>`;
 };
 
-export const reviewsRender=(data)=>{
-
-  const cardsMarkup=data.map(review=>{
-    return `<li class="swiper-slide swiper-slide-review">
-          <img class="review-img" src="${review.avatar_url}" alt="">
+export const reviewsRender = data => {
+  const cardsMarkup = data
+    .map(review => {
+      return `<li class="swiper-slide swiper-slide-review">
+          <img class="review-img" src="${review.avatar_url}" alt="${review.author}"  width="48" height="48">
           <h4 class="review-author">${review.author}</h4>
           <p class="review-text">${review.review}</p>
-        </li>`
-  }).join('');
+        </li>`;
+    })
+    .join('');
 
-
-  const fullMarkup=`<div class="swiper swiper-review">
+  const fullMarkup = `<div class="swiper swiper-review">
       <ul class="swiper-wrapper swiper-wrapper-review">${cardsMarkup}</ul>
     <ul class="buttons-div">
       <li class="swiper-button-prev swiper-button-prev-review">
@@ -36,10 +32,7 @@ export const reviewsRender=(data)=>{
 #review .swiper-button-prev:after, .swiper-button-next:after {
   content: none;
 }
-</style>`
+</style>`;
 
-    
   return fullMarkup;
-
-}
-
+};
