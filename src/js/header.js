85bca4fe5c524ec59bbdbd45c162af099c126ea6
MazaFaaka <haitovvasyl@gmail.com>
2025-04-09 22:47:57 +0200

    fix delate bodyListener of the header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,26 @@
 import refs from '../service/refs';
 
-refs.menuBtn.addEventListener('click', handleOnClick);
-refs.listLink.addEventListener('click', handleRemoveClick);
+refs.menuBtn.addEventListener('click', toggleMenu);
 
-function handleOnClick() {
+function toggleMenu() {
   refs.listLink.classList.toggle('hidden');
+
+  if (!refs.listLink.classList.contains('hidden')) {
+    refs.body.addEventListener('click', closeMenuHandler);
+  } else {
+    refs.body.removeEventListener('click', closeMenuHandler);
+  }
 }
 
-function handleRemoveClick() {
-  refs.listLink.classList.add('hidden');
-}
-refs.body.addEventListener('click', event => {
+function closeMenuHandler(event) {
   if (
     !refs.listLink.contains(event.target) &&
     !refs.menuBtn.contains(event.target)
   ) {
     refs.listLink.classList.add('hidden');
+    refs.body.removeEventListener('click', closeMenuHandler);
+  } else if (event.target.tagName === 'A') {
+    refs.listLink.classList.add('hidden');
+    refs.body.removeEventListener('click', closeMenuHandler);
   }
-});
+}
